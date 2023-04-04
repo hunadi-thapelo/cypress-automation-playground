5381117635df8b1d567f26a8bb9d07ffcd9db5c6
hunadim <hmapulane@gmail.com>
2023-04-04 17:21:23 +0200

    implemented adding to single item to cart

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,7 +8,7 @@ describe('My First Test Suite', function()
     cy.get('.product:visible').should('have.length',4)
     //Parent child chaining
     cy.get('.products').find('.product').should('have.length',4)
-
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
    })
 
 
