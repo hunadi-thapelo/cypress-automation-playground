eda49550e295cca7f765b12a32d988e615a57221
hunadim <hmapulane@gmail.com>
2023-04-04 06:21:37 +0200

    implemented assertion count visible prod element

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,6 +4,8 @@ describe('My First Test Suite', function()
    {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length',4)
 
    })
 
