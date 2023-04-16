8e1a007995d538ca849ec1073af4b869232f8f4c
hunadim <hmapulane@gmail.com>
2023-04-16 09:00:36 +0200

    Dynamically finds a product and adds to cart

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,7 +8,27 @@ describe('My First Test Suite', function()
     cy.get('.product:visible').should('have.length',4)
     //Parent child chaining
     cy.get('.products').find('.product').should('have.length',4)
-    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+    //cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+    //dynamically finding a product, clicking to add to cart
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+        // $el is a wrapped jQuery element
+
+        const vegText = $el.find('h4.product-name').text()
+        if(vegText.includes('Cashews'))
+        {
+            cy.wrap($el).find('button').click()
+        }
+    })
+
+
+
+
+
+
+
+
+
    })
 
 
