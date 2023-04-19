3a05b64145c74f49b3fe126731fd9b13f123d208
hunadim <hmapulane@gmail.com>
2023-04-19 22:33:35 +0200

    unchecks option1 and asserts if unchecked

--- cypress/integration/examples/TestCheckboxes.js
+++ cypress/integration/examples/TestCheckboxes.js
@@ -5,7 +5,12 @@ describe('My First Test Suite', function()
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
     //requirement: check option1, assert if checked, assert if value is option1
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
-    
+
+    //requirement: uncheck option1, assert if unchecked
+    cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+
+
+    //requirement: check multiple checkboxes, option 2 and option 3, assert if checked, assert if value is option1
     
 
 
