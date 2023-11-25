9842e47d4d44f1e1f294ac6cff67bec677471f17
hunadim <hmapulane@gmail.com>
2023-11-25 02:42:23 +0200

    uses Cypress force click method on hidden elements

--- cypress/integration/examples/mouseHoverPopups.js
+++ cypress/integration/examples/mouseHoverPopups.js
@@ -4,9 +4,15 @@ describe('My Seventh Test Suite', function()
    {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
     
-    //jQuery method show to hidden selected elements
-    cy.get('div.mouse-hover-content').invoke('show')
-    cy.contains('Top').click()
+    /*  jQuery method show to hidden selected elements
+        if requirement is to verify the pop up elements when you hover over */
+
+    //cy.get('div.mouse-hover-content').invoke('show') 
+    //cy.contains('Top').click()
+   
+    /* Uses force click option action on invisible or visible element*/
+    
+    cy.contains('Top').click({ force: true })
     cy.url().should('include','top')
 
     })
