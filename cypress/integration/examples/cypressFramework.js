38161c05ce46c01367e178ba1a5ba23b4e967f92
hunadim <hmapulane@gmail.com>
2023-11-30 15:18:13 +0200

    asserts if value dynamically entered is correct

--- cypress/integration/examples/cypressFramework.js
+++ cypress/integration/examples/cypressFramework.js
@@ -7,7 +7,6 @@ describe('Cypress Framework', function()
             this.data=tstData
         })
 
-
       })
 
 
@@ -17,7 +16,8 @@ describe('Cypress Framework', function()
     cy.visit('https://rahulshettyacademy.com/angularpractice/')
     cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
     cy.get('select').select(this.data.gender)
-
+    //requirement: validate if value entered is same as name value entered
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
    })
 
 })
