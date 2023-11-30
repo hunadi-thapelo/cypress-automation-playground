8f0b4fb04caf33943e53c071c05780dba06d6470
hunadim <hmapulane@gmail.com>
2023-11-30 12:43:30 +0200

    WIP to resolve assertion error

--- cypress/integration/examples/iFrames.js
+++ cypress/integration/examples/iFrames.js
@@ -9,5 +9,23 @@ describe('My Cypress iFrames Test Suite', function()
     cy.frameLoaded('#courses-iframe') //gives Cypress knowledge of iframe to focus on / switch to
     cy.iframe().find('a[href*="mentorship"]').eq(0).click() //locator css regular ex
 
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length',2) //assertion //throws an error
+   
+   /* -- WIP to resolve assertion error
+   //cy.iframe().find('button[class*="btn-lg"]').as('price-btn')
+    //cy.iframe().find('@price-btn').should('have.length',2)
+    
+   // find('h1[class*="pricing-title"]').should('have.length',2) //assertion
+
+    //cy.iframe().find('h3[class*="pricing-price"]').should('have.length', 2)
+
+
+     //cy.iframe().find('h3[class*="pricing-price"]').as('price')
+    
+     //cy.find('@price').should('have.length', 2)
+     */
+
+
+
     })
 })
