377cca3b66b4737ba6b3ceff6010bdb09a55f0b5
hunadim <hmapulane@gmail.com>
2023-12-01 09:30:29 +0200

    Modifies home page test into page object pattern

--- cypress/integration/examples/cypressFramework.js
+++ cypress/integration/examples/cypressFramework.js
@@ -1,4 +1,6 @@
 ///<reference types= "cypress" />
+import homePage from '../pageObjects/homePage.js';
+
 
 describe('Cypress Framework', function()
 {
@@ -14,18 +16,27 @@ describe('Cypress Framework', function()
 
    it('My TC with before hook', function()
    {
+    const myHomePage = new homePage()
 
     cy.visit('https://rahulshettyacademy.com/angularpractice/')
-    cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-    cy.get('select').select(this.data.gender)
-    //requirement: validate if value entered is same as name value entered
-    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
-    //requirement: validate if propery min length is correct - simply assertion to check
-    cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength', '2')
-     //requirement: validate if propery is in disabled behvaiour
-    cy.get('#inlineRadio3').should('be.disabled')
 
-    cy.get(':nth-child(2) > .nav-link').click()
+    myHomePage.getNameInputField().type(this.data.name)
+
+    myHomePage.getGender().select(this.data.gender)
+
+    //requirement: validate if value entered is same as name value entered
+    myHomePage.getTwoWayBindingInput().should('have.value', this.data.name)
+
+    //requirement: validate if propery min length is correct - simply assertion to check
+    myHomePage.getNameInputField().should('have.attr','minlength', '2')
+
+    //requirement: validate if propery is in disabled behvaiour
+    myHomePage.getEntreprenuer().should('be.visible')
+
+     
+    //Navigates to Shop ecommerce tab
+    myHomePage.getShopTab().click()
+
     //cy.selectProduct('Samsung Note 8') //selects one product to cart
     
     //Requirement select and add 2 products to cart
