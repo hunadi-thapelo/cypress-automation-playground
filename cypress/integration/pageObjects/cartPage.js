83a5dde0366364fefda88086e32364d963f6b8fe
hunadim <hmapulane@gmail.com>
2023-12-01 17:48:42 +0200

    implements cart page objects

---
+++ cypress/integration/pageObjects/cartPage.js
@@ -0,0 +1,10 @@
+class cartPage{
+
+     getCheckOutSuccess(){
+     return cy.get('[class="btn btn-success"]') //takes to delivery & purchase
+    }
+
+    }
+    
+    
+    export default cartPage;
